3d5545ea7444ffee6ed0162a97ccc0f97e6c8734
klxin <xinkailun@itcast.cn>
2020-01-04 18:57:04 +0800

    电商项目-项目初始化

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <h2>我是首页</h2>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="less" scoped>
+.container {
+  h2 {
+    color: cyan;
+  }
+}
+</style>

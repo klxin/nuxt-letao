94ccc42d6d457ab88c2f85a148f85d16f59735b8
klxin <xinkailun@itcast.cn>
2020-01-04 19:37:10 +0800

    电商项目-轮播图静态结构完成

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,33 @@
 <template>
-  <div class="container">
+  <div class="home">
     <!-- eslint-disable -->
-    <van-button type="primary">主要按钮</van-button>
+    <!-- 轮播图区域 -->
+    <van-swipe :autoplay="3000">
+      <van-swipe-item v-for="(image, index) in images" :key="index">
+        <img :src="image" />
+      </van-swipe-item>
+    </van-swipe>
   </div>
 </template>
 
 <script>
+export default {
+  data () {
+    return {
+      images: [
+        'https://img.yzcdn.cn/vant/apple-1.jpg',
+        'https://img.yzcdn.cn/vant/apple-2.jpg'
+      ]
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
+.home {
+  position: absolute;
+  top: 40px;
+  bottom: 40px;
+  width: 100%;
+}
 </style>

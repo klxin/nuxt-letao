c70bedbb53d1affc03f7bf3539c8424c0e4ed5d0
klxin <xinkailun@itcast.cn>
2020-01-06 10:17:52 +0800

    电商项目-底部Tabbar完成

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,8 +3,26 @@
     <!-- 顶部导航栏 -->
     <van-nav-bar title="移动电商·乐淘" />
     <nuxt />
+    <!-- 底部tabbar -->
+    <!-- eslint-disable -->
+    <van-tabbar v-model="active">
+      <van-tabbar-item icon="home-o">首页</van-tabbar-item>
+      <van-tabbar-item icon="medal-o">会员</van-tabbar-item>
+      <van-tabbar-item icon="cart-circle-o" info="3">购物车</van-tabbar-item>
+      <van-tabbar-item icon="manager-o">我的</van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      active: 0
+    }
+  }
+}
+</script>
 
 <style scoped lang="less">
 .container {
